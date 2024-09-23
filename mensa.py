4d5c7fe1b6d97b4ff11773932ccc407e8d1ff5a6
budelmann <daniel.budelmann@mevis.fraunhofer.de>
2024-09-23 19:13:14 +0200

    Fallback for 404 mcloud

--- mensa.py
+++ mensa.py
@@ -1,4 +1,5 @@
 import logging
+
 logger = logging.getLogger(__name__)
 import requests
 import json
@@ -6,15 +7,22 @@ from menu import Menu
 
 MENSA_URL = "https://studentenwerk.sh/de/mensen-in-luebeck?ort=3&mensa=8#mensaplan"
 
+
 def getMensaMenu(today):
+    menu = Menu("Studenten Mensa", MENSA_URL)
+    
     logger.debug(f"getMensaMenu")
     day = today.strftime("%a")
     refresh()
     url = f"https://speiseplan.mcloud.digital/meals?day={day}"
     response = requests.get(url)
-    menu_dict = json.loads(response.text)[0]
+    if response.status_code == 200:
+        menu_dict = json.loads(response.text)[0]
+    else:
+        logger.debug(f"Failed to download {url}")
+        menu.add_item(f"Konnte {url} nicht erreichen.", "")
+        return menu
 
-    menu = Menu("Studenten Mensa", MENSA_URL)
     for meal in menu_dict["meals"]:
         if meal["location"] == "Mensa":
             menu.add_item(meal["name"], meal["price"])
