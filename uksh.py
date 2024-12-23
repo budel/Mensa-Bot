d0cbf1fd1c8911e465007288a165524398f5c123
budelmann <daniel.budelmann@mevis.fraunhofer.de>
2024-12-23 11:29:40 +0100

    changed function order to be more step down

--- uksh.py
+++ uksh.py
@@ -15,22 +15,6 @@ MFC_URL = "https://www.uksh.de/ssn/Unser+Speisenangebot/Campus+L%C3%BCbeck/MFC+C
 UKSH_URL = "https://www.uksh.de/ssn/Unser+Speisenangebot/Campus+L%C3%BCbeck/UKSH_Bistro+L%C3%BCbeck-p-346.html"
 
 
-def find_pdf(url, today):
-    res = requests.post(url)
-    if res.status_code != 200:
-        return None
-    kw = today.isocalendar()[1]
-    pdf_link = [
-        "https://www.uksh.de" + pdf_link[6:-1]
-        for pdf_link in re.findall('href="[^"]+.pdf"', res.text)
-        if f"KW+{kw:0>2}" in pdf_link
-    ]
-    if pdf_link == []:
-        raise FileNotFoundError(f"Could not find this week's pdf at {url}")
-    logger.info(f"pdf_link for {url}: {pdf_link[0]}")
-    return pdf_link[0]
-
-
 def getMFCMenu(today):
     global logger
     logger = getLogger(__name__ + "_mfc")
@@ -57,21 +41,20 @@ def createMenu(title, url, today):
         return Menu(title, url)
 
 
-def parse_pdf(weekday, menu, filename="menu.pdf", dpi=300, veggie_index=1):
-    logger.debug(f"parse_pdf")
-    with fitz.open(filename) as pdf:
-        assert pdf.page_count == 1
-        page = pdf[0]
-        pil_image = preprocessImage(page, dpi=dpi)
-        rows, ys = extract_weekday_rows(pil_image)
-        y = ys[weekday]
-        cols, xs = extract_menu_cols(rows[weekday])
-        for i, (col, x) in enumerate(zip(cols, xs)):
-            text, price = extract_text(page, x, y, dpi=dpi)
-            if not text or not price:
-                continue
-            menu.add_item(" ".join(text), price, vegetarian=i == veggie_index)
-    return menu
+def find_pdf(url, today):
+    res = requests.post(url)
+    if res.status_code != 200:
+        return None
+    kw = today.isocalendar()[1]
+    pdf_link = [
+        "https://www.uksh.de" + pdf_link[6:-1]
+        for pdf_link in re.findall('href="[^"]+.pdf"', res.text)
+        if f"KW+{kw:0>2}" in pdf_link
+    ]
+    if pdf_link == []:
+        raise FileNotFoundError(f"Could not find this week's pdf at {url}")
+    logger.info(f"pdf_link for {url}: {pdf_link[0]}")
+    return pdf_link[0]
 
 
 def prepare_pdf(url, filename):
@@ -109,6 +92,32 @@ def auto_crop_pdf(input_pdf, output_pdf):
     doc.close()
 
 
+def parse_pdf(weekday, menu, filename="menu.pdf", dpi=300, veggie_index=1):
+    logger.debug(f"parse_pdf")
+    with fitz.open(filename) as pdf:
+        assert pdf.page_count == 1
+        page = pdf[0]
+        pil_image = preprocessImage(page, dpi=dpi)
+        rows, ys = extract_weekday_rows(pil_image)
+        y = ys[weekday]
+        cols, xs = extract_menu_cols(rows[weekday])
+        for i, (col, x) in enumerate(zip(cols, xs)):
+            text, price = extract_text(page, x, y, dpi=dpi)
+            if not text or not price:
+                continue
+            menu.add_item(" ".join(text), price, vegetarian=i == veggie_index)
+    return menu
+
+
+def preprocessImage(page, dpi=300):
+    logger.debug(f"preprocessImage {page}")
+    pix = page.get_pixmap(dpi=dpi)
+    image = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+    # Convert the cropped image to grayscale
+    gray_image = image.convert("L")
+    return gray_image
+
+
 def extract_weekday_rows(pil_image):
     logger.debug(f"extract_weekday_rows {pil_image}")
     row_boundaries = detect_boundaries(np.asarray(pil_image), 1)
@@ -126,15 +135,6 @@ def extract_menu_cols(row):
     return [
         row.crop((left, 0, right, row.height)) for (left, right) in sorted(biggest_cols)
     ], sorted(biggest_cols)
-
-
-def preprocessImage(page, dpi=300):
-    logger.debug(f"preprocessImage {page}")
-    pix = page.get_pixmap(dpi=dpi)
-    image = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
-    # Convert the cropped image to grayscale
-    gray_image = image.convert("L")
-    return gray_image
 
 
 def get_biggest_boundaries(boundaries):
