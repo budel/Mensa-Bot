4f0d759553c87229218c9004b0f342e9b400ab70
budelmann <daniel.budelmann@mevis.fraunhofer.de>
2025-01-07 10:36:35 +0100

    Activated burger menu with updated prices

--- burger.py
+++ burger.py
@@ -7,19 +7,7 @@ class BurgerMenu(Menu):
     def __init__(self, title="", url=""):
         super().__init__(title, url)
         self.items = [
-            MenuItem("Special Menü", "14,00 €"),
-            MenuItem("Special Menü mit Süßkartoffelpommes", "15,00 €"),
-            MenuItem("Cheeseburger", "8,50 €"),
-            MenuItem("Double Cheese & Bacon", "8,90 €"),
-            MenuItem("Französischer Ziegenkäse Burger", "8,90 €"),
-            MenuItem("Feuriger Salsa Burger", "8,90 €"),
-            MenuItem("Original Smoked BBQ-Burger", "9,50 €"),
-            MenuItem("Falafel Burger", "8,50 €"),
-            MenuItem("Kiwi Halloumi Burger", "8,50 €"),
-            MenuItem("Jackfruit Burger", "8,90 €"),
-            MenuItem("Planty of Chicken Burger", "8,90 €"),
-            MenuItem("Rustic Fries", "4,50 €"),
-            MenuItem("Süßkartoffelpommes", "5,50 €"),
+            MenuItem("Dummy", "0,00 €", vegetarian=False, vegan=False),
         ]
 
     def __str__(self):
@@ -32,12 +20,12 @@ class BurgerMenu(Menu):
         <li>✓ Rustic Fries mit Dip
         <li style="display:flex; justify-content:space-between;">
             ✓ Drink
-            <div>14,00 €
+            <div>15,00 €
         </li>
         <br>
         <li style="display:flex; justify-content:space-between;">
             mit Süßkartoffelpommes
-            <div>15,00 €
+            <div>16,00 €
         </li>
     </ul>
 </div>
@@ -45,53 +33,53 @@ class BurgerMenu(Menu):
 <ul style="list-style-type: none; padding: 0;">
     <li style="display:flex; justify-content:space-between;">
         Cheeseburger
-        <div>8,50 €
+        <div>9,00 €
     </li>
     <li style="display:flex; justify-content:space-between;">
         Double Cheese & Bacon
-        <div>8,90 €
+        <div>9,50 €
     </li>
     <li style="display:flex; justify-content:space-between;">
         <div><div>Französischer Ziegenkäse Burger</div>
         <small>mit Feigen Senf-Sauce</small></div>
-        <div>8,90 €
+        <div>9,50 €
     </li>
     <li style="display:flex; justify-content:space-between;">
         <div><div>Feuriger Salsa Burger</div>
         <small>mit Jalapenos</small></div>
-        <div>8,90 €
+        <div>9,50 €
     </li>
     <li style="display:flex; justify-content:space-between;">
         <div><div>Original Smoked BBQ-Burger</div>
         <small>mit Bacon</small></div>
-        <div>9,50 €
+        <div>10,00 €
     </li>
     <br>
     <strong>Veggie & Vegan</strong>
     <li style="display:flex; justify-content:space-between;">
-        Falafel Burger
-        <div>8,50 €
+        &#127793;Falafel Burger
+        <div>9,00 €
     </li>
     <li style="display:flex; justify-content:space-between;">
-        Kiwi Halloumi Burger
-        <div>8,50 €
+        &#127793;Kiwi Halloumi Burger
+        <div>9,00 €
     </li>
     <li style="display:flex; justify-content:space-between;">
-        Jackfruit Burger
-        <div>8,90 €
+        &#127793;Jackfruit Burger
+        <div>9,50 €
     </li>
     <li style="display:flex; justify-content:space-between;">
-        Planty of Chicken Burger
-        <div>8,90 €
+        &#127793;Planty of Chicken Burger
+        <div>10,00 €
     </li>
     <br>
     <strong>Beilagen mit Dip</strong>
     <li style="display:flex; justify-content:space-between;">
-        Rustic Fries
+        &#127793;Rustic Fries
         <div>4,50 €
     </li>
     <li style="display:flex; justify-content:space-between;">
-        Süßkartoffelpommes
+        &#127793;Süßkartoffelpommes
         <div>5,50 €
     </li>
 </ul>
@@ -107,4 +95,4 @@ def getBurgerMenu(today):
 
 
 def isBurgerDay(today):
-    return today.strftime("%a") == "Tue"
+    return today.weekday() == 1
