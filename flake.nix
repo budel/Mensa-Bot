72f7f9786544e4c4313633381e1a6ad50cefa045
budelmann <daniel.budelmann@mevis.fraunhofer.de>
2025-03-03 10:39:23 +0000

    Use uv in a simple flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Python development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            uv
+          ];
+        };
+      });
+}
