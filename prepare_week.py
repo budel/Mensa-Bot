4d2ee1125aa1b241378ff6f6b278c401f2e05eff
dbud <daniel.budelmann@mevis.fraunhofer.de>
2025-03-27 09:31:00 +0100

    prepare previews for the whole week

---
+++ prepare_week.py
@@ -0,0 +1,46 @@
+import datetime
+import json
+import os
+import pathlib
+
+from burger import getBurgerMenu
+from mensa import getMensaMenu
+from preview import preview_menus
+from uksh import getMFCMenu, getUKSHMenu
+
+
+def main():
+    menus_file = parse_week()
+    with open(menus_file, "r") as f:
+        menus_list = json.load(f)
+    for menus_dict in menus_list:
+        preview_menus(menus_dict)
+
+
+def parse_week():
+    today = datetime.date.today()
+    monday = today - datetime.timedelta(days=today.weekday())
+
+    menus_file = (
+        pathlib.Path(__file__).parent.resolve() / "menus" / f"menus-{monday}.json"
+    )
+    if os.path.exists(menus_file):
+        return menus_file
+
+    menu_list = []
+    for i in range(5):
+        day = monday + datetime.timedelta(days=i)
+        menus_per_day = []
+        for menu_fn in [getMFCMenu, getUKSHMenu, getMensaMenu, getBurgerMenu]:
+            menu = menu_fn(day)
+            menus_per_day.append(menu)
+        menu_list.append([menu.to_dict() for menu in menus_per_day])
+
+    with open(menus_file, "w", encoding="utf-8") as f:
+        json.dump(menu_list, f, indent=2, ensure_ascii=False)
+
+    return menus_file
+
+
+if __name__ == "__main__":
+    main()
--- preview.py
+++ preview.py
@@ -1,59 +1,58 @@
-import json
 import os
-import pathlib
 
 import requests
 from dotenv import load_dotenv
 
 from menu import Menu
 
-load_dotenv()
-cookie = os.getenv("COOKIE")
-url = os.getenv("GENAI_URL")
-headers = {
-    "Cookie": f"AppServiceAuthSession={cookie}",
-}
 
-menus_file = pathlib.Path(__file__).parent.resolve() / "menus.json"
-with open(menus_file, "r") as f:
-    menus_dict = json.load(f)
-for menu_dict in menus_dict:
-    menus = Menu.from_dict(menu_dict)
-    for item in menus.items:
-        meal = item.name
-        print(meal)
+def preview_menus(menus_dict):
+    load_dotenv()
+    cookie = os.getenv("COOKIE")
+    url = os.getenv("GENAI_URL")
+    headers = {
+        "Cookie": f"AppServiceAuthSession={cookie}",
+    }
 
-        if f"{meal}.png" in os.listdir("meals"):
-            continue
+    for menu_dict in menus_dict:
+        menus = Menu.from_dict(menu_dict)
+        for item in menus.items:
+            meal = item.name
+            print(meal)
 
-        payload = {
-            "messages": [
-                {
-                    "content": meal,
-                    "role": "user",
-                }
-            ],
-            "context": {
-                "overrides": {
-                    "model": "dalle3",
-                    "imageSize": "1792x1024",
-                    "style": "vivid",
-                    "quality": "hd",
-                }
-            },
-        }
+            if f"{meal}.png" in os.listdir("meals"):
+                continue
 
-        response = requests.post(url, json=payload, headers=headers)
+            payload = {
+                "messages": [
+                    {
+                        "content": meal,
+                        "role": "user",
+                    }
+                ],
+                "context": {
+                    "overrides": {
+                        "model": "dalle3",
+                        "imageSize": "1792x1024",
+                        "style": "vivid",
+                        "quality": "hd",
+                    }
+                },
+            }
 
-        if response.status_code == 200:
-            res = response.json()
-            image_url = res["choices"][0]["url"]
-            response = requests.get(image_url)
+            response = requests.post(url, json=payload, headers=headers)
+
             if response.status_code == 200:
-                with open(f"meals/{meal}.png", "wb") as file:
-                    file.write(response.content)
-                print("Image downloaded successfully.")
+                res = response.json()
+                image_url = res["choices"][0]["url"]
+                response = requests.get(image_url)
+                if response.status_code == 200:
+                    with open(f"meals/{meal}.png", "wb") as file:
+                        file.write(response.content)
+                    print("Image downloaded successfully.")
+                else:
+                    print(
+                        "Failed to download image. Status code:", response.status_code
+                    )
             else:
-                print("Failed to download image. Status code:", response.status_code)
-        else:
-            print("Error:", response.status_code, response.text)
+                print("Error:", response.status_code, response.text)
