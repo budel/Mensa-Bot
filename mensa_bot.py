4da8a02b83f7676118637f3f403db089b6f96e5e
budel <Budelchen@gmail.com>
2025-07-01 18:22:34 +0200

    Fetch json from github and parse it once daily instead of creating it again

--- mensa_bot.py
+++ mensa_bot.py
@@ -1,22 +1,19 @@
 import datetime
-import json
 import logging
-import pathlib
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
     filename="mensa_bot.log", encoding="utf-8", filemode="w+", level=logging.DEBUG
 )
 import os
-import sys
+import requests
 
 from dotenv import load_dotenv
 
-from burger import getBurgerMenu
-from mensa import getMensaMenu
 from menu import Menu
 from message import Message
-from uksh import getMFCMenu, getUKSHMenu
+
+MENUS_URL = "https://budel.github.io/Mensa-Bot/menus.json"
 
 
 def main():
@@ -31,7 +28,9 @@ def main():
     )
 
     # create sections in message
-    menus = create_message(message)
+    response = requests.get(MENUS_URL)
+    data = response.json()
+    parse_menu(message, data)
 
     # create a link to the repo
     message.addSection(
@@ -41,76 +40,20 @@ def main():
     )
 
     message.printme()
-    menus_file = pathlib.Path(__file__).parent.resolve() / "public" / "menus.json"
-    menu_list = [menu.to_dict() for menu in menus]
-    if len(sys.argv) > 1:
-        check_for_updates(menu_list, menus_file)
-    else:
-        if message.isValid():
-            message.send()
-    with open(menus_file, "w", encoding="utf-8") as f:
-        json.dump(menu_list, f, indent=2, ensure_ascii=False)
+    if message.isValid():
+        message.send()
 
 
-def create_message(message):
-    logger.debug(f"create_message")
-    today = datetime.date.today()
-    menus = []
-    for menu_fn in [getMFCMenu, getMensaMenu, getUKSHMenu, getBurgerMenu]:
-        menu = menu_fn(today)
-        menus.append(menu)
-        if not menu.is_empty():
-            message.addSection(str(menu))
-    return menus
+def parse_menu(message, all_menus):
+    logger.debug(f"parse_menu")
+    today = datetime.date.today().isoformat()
 
-
-def check_for_updates(menu_list, menus_file="menus.json", send_on_diff=True):
-    logger.debug(f"check_for_updates")
-    if os.path.exists(menus_file):
-        with open(menus_file, "r") as f:
-            prev_menus = json.load(f)
-            for prev, cur in zip(prev_menus, menu_list):
-                prev = Menu.from_dict(prev)
-                cur = Menu.from_dict(cur)
-                if not prev.is_same(cur):
-                    if send_on_diff:
-                        send_correction(what_is_different(cur, prev))
-                    else:
-                        return what_is_different(cur, prev)
-
-
-def what_is_different(cur, prev):
-    logger.debug(f"what_is_different")
-    what = f"<h2><a href={prev.url}>{prev.title}</a></h2>"
-    what += "\n<ul>"
-    for item in prev.items:
-        if cur.has_item(item):
-            what += "\n<li> " + str(item) + "\n"
-            cur.remove_item(item)
-        else:
-            what += "\n<li> <s>" + str(item) + "</s>  \n"
-
-    # generate the new output
-    idx = 0
-    for item in cur.items:
-        new_line = "<b>" + str(item) + "</b>\n"
-        idx = what.find("</s>  \n", idx)
-        offset = len("</s>  \n")
-        if idx != -1:
-            what = what[: idx + offset] + "<br>" + new_line + what[idx + offset :]
-            idx += offset + len("<br>" + new_line)
-        else:
-            what += "<li>" + new_line
-
-    what += "\n</ul>"
-    return what
-
-
-def send_correction(text):
-    logger.debug(f"send_correction")
-    message = Message(os.getenv("WEBHOOK"))
-    message.addSection(f"Korrektur:<br>{text}")
-    message.send()
+    for daily_menus in all_menus:
+        for menu_dict in daily_menus:
+            menu = Menu.from_dict(menu_dict)
+            isTodaysMenu = any(item.date == today for item in menu.items)
+            if isTodaysMenu and not menu.is_empty():
+                message.addSection(str(menu))
 
 
 if __name__ == "__main__":
