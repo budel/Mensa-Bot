ea29c129ba8219c45f31d06e3bad55ba7e2f1c56
budel <Budelchen@gmail.com>
2025-08-10 11:03:37 +0200

    free rust ptr from python

--- fetch_mensa.py
+++ fetch_mensa.py
@@ -2,17 +2,15 @@ import ctypes
 import functools
 
 lib = ctypes.CDLL('fetch_mensa/target/debug/libmensa_fetcher.so')
-lib.compute.restype = ctypes.c_char_p
+lib.compute.restype = ctypes.c_void_p
+lib.free_string.argtypes = [ctypes.c_void_p]
 
 @functools.cache
 def fetch_mensa():
     ptr = lib.compute()
     result = ctypes.c_char_p(ptr).value.decode("utf-8")
+    lib.free_string(ptr)
     if result:
         return result
     else:
         raise RuntimeError("fetch_menu returned an empty string")
-
-# Free the memory in Rust after usage.
-# lib.free_string.argtypes = [ctypes.c_char_p]
-# lib.free_string(ptr)
